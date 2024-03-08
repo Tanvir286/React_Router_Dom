8e02aa339ae4f1df908646d6847c2f4b9aaa59a1
Tanvir286 <tanvirdiu200@gmail.com>
2024-03-08 11:41:26 +0600

    react js

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>           
+             <h1 className='text-6xl text-center mb-96'>About Page</h1>         
+        </>
+    );
+};
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
+
+const Contact = () => {
+    return (
+        <>
+            <Navbar/>
+             <h1 className='text-6xl text-center mb-96'>Contact</h1>
+            <Footer/>
+        </>
+    );
+};
+
+export default Contact;
